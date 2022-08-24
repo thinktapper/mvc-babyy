9a9e147ebd533052e5ee8ade4dbd2a794cb812cb
Andrew Tapper <andrew@tapper.codes>
2022-08-24 13:04:15 -0500

    :ambulance: Derpy hooves

--- src/slides/End.js
+++ src/slides/End.js
@@ -6,7 +6,7 @@ const ThankYou = () => {
           Modularity
         </li>
         <li className="animateIn">
-          Resuablility
+          Reusablility
         </li>
         <li className="animateIn">
           Organized code
